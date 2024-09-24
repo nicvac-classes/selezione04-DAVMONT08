9e3d82c0719ce15384938a52d414c53f1d66fd4f
DAVMONT08 <davide.montella@marconibari.edu.it>
2024-09-24 19:24:44 +0000

    CONSEGNA

--- main.cpp
+++ main.cpp
@@ -2,10 +2,37 @@
 using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+            float n1,n2,n3,nmax;
+            cout<< "inserisci tre numeri"<<endl;
+            cin>>n1;
+            cin>>n2;
+            cin>>n3;
+            if(n1>n2)
+            {
+                nmax=n1;
+            }
+         if(n1>n3)
+         {
+            nmax=n1;
+        }
+     {
+        nmax=n2;
+        }  
+        {
+            nmax=n2;
+            }
+            if(n2>n3)
+            {
+                nmax=n2;
+                }
+             {
+                 nmax=n3;
+                }
+                cout<<"il massimo tra questi tre numeri è" << nmax <<endl;
+
+
+
 }
+
 
 //LEGGERE LE ISTRUZIONI NEL FILE README.md
